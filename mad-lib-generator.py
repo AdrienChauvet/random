4b870b116961214ab65103ef17b6e0b2b0a80895
Adrien Chauvet <adr.chauvet@gmail.com>
2017-02-28 15:51:05 +0100

    Create mad-lib-generator.py

---
+++ mad-lib-generator.py
@@ -0,0 +1,14 @@
+print("\n\t *** Mad Lib Generator ***\n")
+
+a = input("Enter a color: ")
+b = input("Enter a superlative, ending is est: ")
+c = input("Enter an adjective: ")
+d = input("Enter a body part (plural): ")
+e = input("Enter a body part: ")
+f = input("Enter a noun: ")
+g = input("Enter an animal (plural): ")
+h = input("Enter an adjective: ")
+i = input("Enter an adjective: ")
+j = input("Enter an adjective: ")
+
+print("\nThe {0} Dragon is the {1} Dragon of all. It has {2} {3}, and a {4} shaped like a {5}. It loves to eat {6}, although it will feast on nearly anything. It is {7} and {8}. You must be {9} around it, or you may end up as its meal!\n".format(a, b, c, d , e, f, g, h, i, j))
